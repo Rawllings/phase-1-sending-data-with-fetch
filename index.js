048bb66717583168bd116f696d97a8e4ab9835a5
Rawllings Kiprop <rutojr01@gmail.com>
2022-12-06 21:49:20 +0300

    completed task

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
-// fetch("http://localhost:3000/users", {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//     Accept: "application/json",
-//   },
-//   body: JSON.stringify({
-//     name: "Byron",
-//     email: "Poodle",
-//   }),
-// })
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (object) {
-//     console.log(object);
-//   });
-
-const submitData = {
-    name: "Rawllings",
-    email: "rktallam@gmail.com",
-  };
-
-fetch("http://localhost:3000/users", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-  body: JSON.stringify(submitData),
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+            name,
+            email,
+        } )
+    } )
+        .then( function ( response ) {
+            return response.json()
+        } )
+        .then( function ( object ) {
+            document.body.innerHTML = object[ "id" ]
+        } )
+        .catch( function ( error ) {
+            document.body.innerHTML = error.message
+        } )
+}
